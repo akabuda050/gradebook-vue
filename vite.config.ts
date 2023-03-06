f96d83c6c6a1449c820bf833d902c55ac1c51cd1
Olexandr Dolbin <olexandr.dolbin@gmail.com>
2023-03-06 15:05:22 +0100

    make it workong

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,6 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   base: './', // used for github pages. can be removed or changed later.
   plugins: [vue()],
-  server: {
-    host: 'localhost',
-  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
